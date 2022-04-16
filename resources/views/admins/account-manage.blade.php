feb07d3a34e553a39ddcd091834a2948c59e2a8e
huy26062002 <nonemu211@gmail.com>
2022-04-16 16:24:07 +0700

    update account manage

--- resources/views/admins/account-manage.blade.php
+++ resources/views/admins/account-manage.blade.php
@@ -37,7 +37,7 @@
     <div class="col-md-12">
         <div class="page-header clearfix">
             <br><br>
-            <h2 class="pull-left">Sports Details</h2>
+            <h2 class="pull-left">User Account Details</h2>
         </div>
 
 
@@ -45,30 +45,34 @@
         <?php
             $row = [
                 'id' => '1',
-                'category' => 'abc',
-                'name' => 'loc',
+                'username' => 'loc',
+                'email' => 'loc@gmail.com',
+                'password' => '123',
             ];
             if(true) {
                 echo "<table class='table table-bordered table-striped'>";
                     echo "<thead>";
                         echo "<tr>";
-                            echo "<th>#</th>";                                        
-                            echo "<th>Sports Category</th>";
-                            echo "<th>Sports Name</th>";
+                            echo "<th>Id</th>";                                        
+                            echo "<th>Username</th>";
+                            echo "<th>Email</th>";
+                            echo "<th>Password</th>";
                             echo "<th>Action</th>";
                         echo "</tr>";
                     echo "</thead>";
                     echo "<tbody>";
                     for ($i = 0; $i < 3; $i++) {
-                        echo "<tr>";
+                             echo "<tr>";
                             echo "<td>" . $row['id'] . "</td>";                                        
-                            echo "<td>" . $row['category'] . "</td>";
-                            echo "<td>" . $row['name'] . "</td>";
+                            echo "<td>" . $row['username'] . "</td>";
+                            echo "<td>" . $row['email'] . "</td>";
+                            echo "<td>" . $row['password'] . "</td>";
                             echo "<td>";
                             echo "<a href='index.php?act=update&id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><i class='fa fa-edit'></i></a>";
                             echo "  ";
                             echo "<a href='index.php?act=delete&id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><i class='fa fa-trash'></i></a>";
                             echo "</td>";
+
                         echo "</tr>";
                     }
                     echo "</tbody>";                            
