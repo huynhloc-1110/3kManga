de33c659257f568e5b6d216530fd77c97665a5d6
huy26062002 <nonemu211@gmail.com>
2022-04-16 16:37:20 +0700

    change to blade syntax

--- resources/views/admins/account-manage.blade.php
+++ resources/views/admins/account-manage.blade.php
@@ -40,47 +40,44 @@
             <h2 class="pull-left">User Account Details</h2>
         </div>
 
-
-
-        <?php
+        @php
             $row = [
                 'id' => '1',
                 'username' => 'loc',
                 'email' => 'loc@gmail.com',
                 'password' => '123',
             ];
-            if(true) {
-                echo "<table class='table table-bordered table-striped'>";
-                    echo "<thead>";
-                        echo "<tr>";
-                            echo "<th>Id</th>";                                        
-                            echo "<th>Username</th>";
-                            echo "<th>Email</th>";
-                            echo "<th>Password</th>";
-                            echo "<th>Action</th>";
-                        echo "</tr>";
-                    echo "</thead>";
-                    echo "<tbody>";
-                    for ($i = 0; $i < 3; $i++) {
-                             echo "<tr>";
-                            echo "<td>" . $row['id'] . "</td>";                                        
-                            echo "<td>" . $row['username'] . "</td>";
-                            echo "<td>" . $row['email'] . "</td>";
-                            echo "<td>" . $row['password'] . "</td>";
-                            echo "<td>";
-                            echo "<a href='index.php?act=update&id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><i class='fa fa-edit'></i></a>";
-                            echo "  ";
-                            echo "<a href='index.php?act=delete&id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><i class='fa fa-trash'></i></a>";
-                            echo "</td>";
+        @endphp
 
-                        echo "</tr>";
-                    }
-                    echo "</tbody>";                            
-                echo "</table>";
-            } else{
-                echo "<p class='lead'><em>No records were found.</em></p>";
-            }
-        ?>
+        @if (true)
+       <table class='table table-bordered table-striped'>
+            <thead>
+                <tr>
+                    <th>Id</th>                                      
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            @for ($i = 0; $i < 3; $i++) 
+                <tr>
+                <td>{{ $row['id'] }}</td>                                        
+                <td>{{ $row['username'] }}</td>
+                <td>{{ $row['email'] }}</td>
+                <td>{{ $row['password'] }}</td>
+                <td>
+                <a href='index.php?act=update&id="{{ $row['id'] }}"' title='Update Record' data-toggle='tooltip'><i class='fa fa-edit'></i></a>
+                <a href='index.php?act=delete&id="{{ $row['id'] }}"' title='Delete Record' data-toggle='tooltip'><i class='fa fa-trash'></i></a>
+                </td>
+                </tr>
+            @endfor
+            </tbody>                            
+        </table>
+        @else
+            <p class='lead'><em>No records were found.</em></p>
+        @endif
     </div>
 </div> 
 @endsection
