eef90752c3d23247c4f153511b8290af668a2ace
huynhloc-1110 <le1999loc@gmail.com>
2022-04-18 21:29:46 +0700

    Update content in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.master')
 
+@section('asset')
+    @parent
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+@endsection
+
 @section('title', 'Home')
     
 @section('content')
-<div>
-    <p>This is the home page</p>
+@for ($i = 0; $i < 2; $i++)
+<div class="row">
+    @for ($j = 0; $j < 4; $j++)
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box bg-info">
+        <div class="card border-0 bg-light mb-2">
+            <img src="dist/img/one_piece.jpg" alt="one_piece_cover">
+        </div>
+        <a href="#" class="small-box-footer">One Piece <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    @endfor
 </div>
+@endfor
 @endsection
