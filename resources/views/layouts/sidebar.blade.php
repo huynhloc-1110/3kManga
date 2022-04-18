b4c7de86733995976478008dbf2233a98b427161
huynhloc-1110 <le1999loc@gmail.com>
2022-04-18 19:34:24 +0700

    Update the link in sidebar view

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
+    <a href="{{ url('home') }}" class="brand-link">
       <img src="dist/img/3kMangaLogo.png" alt="3kManga Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">3kManga</span>
     </a>
@@ -34,7 +34,7 @@
           </li>
           <!-- Library Menu Item -->
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+            <a href="{{ url('library') }}" class="nav-link">
             <i class="nav-icon fas fa-bookmark"></i>
               <p>
                 Library
@@ -43,7 +43,7 @@
           </li>
           <!-- Update Menu Item -->
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+            <a href="{{ url('update') }}" class="nav-link">
             <i class="nav-icon fas fa-upload"></i>
               <p>
                 Update
