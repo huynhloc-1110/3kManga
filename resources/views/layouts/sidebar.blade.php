474aab5714a0903e7c51b16fc2e580d227b323fc
huynhloc-1110 <le1999loc@gmail.com>
2022-04-19 22:15:42 +0700

    Update home link and web logo link in the navbar layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('home') }}" class="brand-link">
+    <a href="{{ url('') }}" class="brand-link">
       <img src="dist/img/3kMangaLogo.png" alt="3kManga Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">3kManga</span>
     </a>
@@ -25,7 +25,7 @@
                with font-awesome or any other icon font library -->
           <!-- Home Menu Item -->
           <li class="nav-item">
-            <a href="{{ url('home') }}" class="nav-link">
+            <a href="{{ url('') }}" class="nav-link">
               <i class="nav-icon fas fa-house-user"></i>
               <p>
                 Home
