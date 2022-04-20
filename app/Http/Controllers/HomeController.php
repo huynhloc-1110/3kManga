8c68570ef7f2347ecf1a1d356392191a854a3038
huynhloc-1110 <le1999loc@gmail.com>
2022-04-20 09:49:14 +0700

    Update search function to HomeController
    Modify HomeController to check for 'search' input in request and change the eloqent query based on that

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,14 @@ use App\Models\Manga;
 
 class HomeController extends Controller
 {
-    public function showMangas() {
-        $mangas = Manga::all();
+    public function showMangas(Request $request) {
+        $search = $request->input('search');
+        if (isset($search)) {
+            $mangas = Manga::where('name', 'like', '%'.$search.'%')->get();
+        }
+        else {
+            $mangas = Manga::all();
+        }
         return view('home', compact('mangas'));
     }
 }
