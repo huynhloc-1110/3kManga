0935976d3a45196fa2b7c83fec384cb98a833047
huynhloc-1110 <le1999loc@gmail.com>
2022-04-21 16:47:02 +0700

    Add comments to LoginController, change error appearance in login view

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,19 +12,26 @@ class LoginController extends Controller
     }
 
     public function authenticate(Request $request) {
+        //validate first
         $credentials = $request->validate([
             'email'=>'required|email',
             'password'=>'required|min:5|max:14',
         ]);
 
+        //check login credentials with database
         if (Auth::attempt($credentials, true)) {
             $request->session()->regenerate();
+            
+            //put authenticated user info into session for later use in views
             session(['user-info' => Auth::user()]);
+
+            //redirect to home view
             return redirect()->intended('/');
         }
         
+        //if login credentials is inccorect, return to login view with login errors and email input
         return back()->withErrors([
-            'email' => 'Incorrect login credentials.'
+            'login' => 'Incorrect login credentials.'
         ])->onlyInput('email');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -28,17 +28,19 @@
                 <form method="post" class="register-form" id="login-form" action="{{ url('login-submit') }}">
                     @csrf
                     <!-- Email -->
+                    <span style="color:red;" >@error('email'){{$message}}@enderror</span>
                     <div class="form-group">
                         <label for="your_email"><i class="zmdi zmdi-email"></i></label>
-                        <input type="email" name="email" id="email" placeholder="Your Email" value="{{old('password')}}"/>
+                        <input type="email" name="email" id="email" placeholder="Your Email" value="{{ old('email') }}"/>
                     </div>
                     <!-- Password -->
+                    <span style="color:red;" >@error('password'){{$message}}@enderror</span>
                     <div class="form-group">
                         <label for="password"><i class="zmdi zmdi-lock"></i></label>
                         <input type="password" name="password" id="password" placeholder="Password"/>
                     </div>
                     <!-- Error -->
-                    <span style="color:red;" >@error('email'){{$message}}@enderror</span>
+                    <span style="color:red;" >@error('login'){{$message}}@enderror</span>
                     <!-- Submit button -->
                     <div class="form-group form-button">
                         <input type="submit" name="user" id="login" class="form-submit" value="Log in"/>
